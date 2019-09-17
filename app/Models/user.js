aeb8440c4995906007c2198a06881e8bde2c5a4e
Domey Ben <domeybenjamin1@gmail.com>
2019-09-17 20:37:36 +1200

    work

--- app/Models/user.js
+++ app/Models/user.js
@@ -39,4 +39,6 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('User', UserSchema)
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
